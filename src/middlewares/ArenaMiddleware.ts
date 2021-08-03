52316a353f6b195e22aba507f9844ebf67f517f3
Alex Do <alex.donh@gmail.com>
2021-08-03 13:03:35 +0700

    Fix redis connection

--- src/middlewares/ArenaMiddleware.ts
+++ src/middlewares/ArenaMiddleware.ts
@@ -1,14 +1,14 @@
 import {Constant, IMiddleware, Inject, Middleware} from "@tsed/common";
 import BullArena from "bull-arena";
 import BeeQueue from "bee-queue";
-import {RedisClient} from "redis";
 import {QueueOptions, QueueSettings} from "../interfaces";
 import {Store} from "@tsed/core";
+import {RedisService} from "tsed-redis";
 
 @Middleware()
 export class ArenaMiddleware implements IMiddleware {
-  @Inject(RedisClient)
-  private redis: RedisClient;
+  @Inject()
+  private redisService: RedisService;
 
   @Constant("queue")
   private queueSettings: QueueSettings;
@@ -17,11 +17,15 @@ export class ArenaMiddleware implements IMiddleware {
     const queues =
       this.queueSettings.providers?.map((q) => {
         const {name, hostId} = Store.from(q)?.get("queue") as QueueOptions;
+        const redis: any =
+          !this.queueSettings.redis || typeof this.queueSettings.redis === "string"
+            ? this.redisService.get(hostId || this.queueSettings.redis) // use singleton di to avoid creating too much connections
+            : this.queueSettings.redis;
         return {
           name,
           hostId: hostId || "default",
           type: "bee",
-          redis: this.redis as any // use singleton di to avoid creating too much connections
+          redis
         };
       }) || [];
     return BullArena(
